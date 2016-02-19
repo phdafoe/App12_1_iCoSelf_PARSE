b68861c105d3d11a328e3badce300cca8164f770
phdafoe <phdafoe@gmail.com>
2016-02-19 17:12:50 +0100

    Initial Commit

---
+++ App_iCoSelf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App_iCoSelf
+//
+//  Created by User on 19/2/16.
+//  Copyright © 2016 iCologic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
